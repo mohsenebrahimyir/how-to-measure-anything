6a2738b1bd4105ed311f1176775317486f338021
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-06-02 03:00:02 +0430

    add R, Rmd, yaml, html, js, css files

---
+++ R/library.R
@@ -0,0 +1,22 @@
+## General ---------------------------------------------------------------------
+### %>%, ggplot, ....
+library(tidyverse)
+
+### output html, pdf, tex, word & ...
+library(knitr)
+
+### table 
+
+### Plot
+
+## Date and Time ---------------------------------------------------------------
+library(lubridate)
+
+## Date Convert ----------------------------------------------------------------
+library(ConvCalendar)
+# as.OtherDate(x, format="%d%b%Y", calendar = "persian")
+
+## fontawsome and emoji --------------------------------------------------------
+library(fontawesome)
+# emo::ji("poop")
+
